65f946ede73382ed2bba7c438a4b4a620343bd80
L Abhinav Varma <67184999+labhinavvarma@users.noreply.github.com>
2025-06-12 11:56:41 -0400

    Update invoke_router.py

--- invoke_router.py
+++ invoke_router.py
@@ -1,1 +1,31 @@
-
+{
+  "requestID": "1",
+  "processStatus": {
+    "completed": "false",
+    "isMemput": "false",
+    "errorCode": null,
+    "errorText": null
+  },
+  "consumer": [
+    {
+      "firstName": "JUNEY",
+      "lastName": "TROR",
+      "middleName": null,
+      "sex": "F",
+      "dob": "196971109",
+      "addressList": [
+        {
+          "type": "P",
+          "zip": null
+        }
+      ],
+      "id": {
+        "ssn": null
+      }
+    }
+  ],
+  "searchSetting": {
+    "minScore": "100",
+    "maxResult": "1"
+  }
+}
